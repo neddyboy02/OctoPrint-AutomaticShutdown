167a04e03022c175935529304585f7c4aaf57346
neddyboy02 <jimneedham@me.com>
2017-04-29 08:34:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "automaticshutdown"
+plugin_identifier = "printershutdown"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_automaticshutdown"
+plugin_package = "printer_automaticshutdown"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-AutomaticShutdown"
+plugin_name = "Printer-AutomaticShutdown"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.4"
