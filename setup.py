8b59260ec1c4102680393fdff17e3b61395ceb3d
neddyboy02 <jimneedham@me.com>
2017-04-29 08:23:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ plugin_version = "0.1.4"
 plugin_description = "Enables the system to be automatically shut down after a print is finished"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Nicanor Romero Venier"
+plugin_author = "Jim Needham"
 
 # The plugin's author's mail address.
 plugin_author_email = "nicanor.romerovenier@bq.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/OctoPrint/OctoPrint-AutomaticShutdown"
+plugin_url = "https://github.com/neddyboy02/OctoPrint-AutomaticShutdown.git"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
